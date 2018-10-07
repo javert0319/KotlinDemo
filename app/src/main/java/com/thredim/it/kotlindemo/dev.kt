5848653ee9be877b5f709ec47fc08a829d73ee5e
j国士无双w <chia021@163.com>
2018-10-07 20:03:22 +0800

    上传Dev

--- app/src/main/java/com/thredim/it/kotlindemo/dev.kt
+++ app/src/main/java/com/thredim/it/kotlindemo/dev.kt
@@ -2,11 +2,11 @@ package com.thredim.it.kotlindemo
 
 /**
  * @company Thredim
- * @date on 2018/9/21.
+ * @date on 2018/10/7.
  * @author JAIWEI
  * @org www.thredim.com (宁波视睿迪光电有限公司)
  * @email thredim@thredim.com
  * @describe 添加描述
  */
-class dev {
+class Dev {
 }
